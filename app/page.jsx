fc39edebfe6b11124477483a6aaed589551009ec
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-15 20:29:00 -0600

    page home

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <div className=''>
-      <article>
-        <h1>Bootstrap 5 theme crafted by <strong>Theme Wagon</strong></h1>
-        <h6>We've spent the last 7 years helping over 1 million people like yourself to create stunning websites with minimum effort</h6>
-      </article>
-    </div>
+    <>
+    <div>
+      <div id='home' className='flex justify-center items-center h-full w-full'>
+        <article className='flex justify-end h-full'>
+          <div className='w-11/12 text-end'>
+            <h1 id='title' className='text-[2rem] font-semibold mr-2'>Bootstrap 5 theme</h1>
+            <h2 className='text-2xl'>crafted by <strong>Theme Wagon</strong></h2>
+            <div className='flex justify-end'>
+              <p className='mt-2 w-[19rem] text-start ml- text-[.6rem] '>We've spent the last 7 years helping over 1 million people like yourself to create stunning websites with minimum effort</p>
+            </div>
+            <div className='flex justi'>
+              <button id='btndemo' type='button' className='text border text-xs w-20 h-6 rounded-lg mt-8 text-white'>Click Demo</button>
+            </div>
+          </div>
+        </article>
+        <section className=' h-full w-9/12 flex justify-end'>
+          <Image className='w-full h-full' src="/illustration1.png" width="1280" height="780" alt='image' />
+        </section>
+        
+      </div>
+      <footer className='w-full flex justify-center' >
+        <div className='h-20 w-10/12 flex justify-between items-center mx-40'>
+          <Image className='w-12 h-5' src="/google.png" width="500" height="300" alt='image' />
+          <Image className='w-16 h-5' src="/netflix.png" width="500" height="300" alt='image' />
+          <Image className='w-16 h-5' src="/microsoft.png" width="500" height="300" alt='image' />
+          <Image className='w-20 h-5' src="/mailbuster.png" width="500" height="300" alt='image' />
+          <Image className='w-12 h-5' src="/themewagon.png" width="500" height="300" alt='image' />
+        </div>
+      </footer>
+      </div>
+     
+    </>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
     return (
-        <nav className='flex justify-center h-14 w-full bg-gradient-to-r from-transparent via-white'>
-            <ul className='w-full'>
-                <div className='flex items-center justify-center gap-40 w-full'>
-                    <div className='flex gap-8'>
+        <nav className='flex h-14 w-full bg-gradient-to-r from-transparent via-white to-transparent'>
+
+            <ul className='w-full text-[.65rem]'>
+                <div id='menu' className='flex items-center w-full justify-start  px-10 py-3'>
+                    <div className='flex items-center justify-center'>
+                        <Image src="/icon.png" width='54' height='54' alt='logo'/>
+                        <span id='klean' className='font-semibold text-[1rem]'>Klean</span>
+                    </div>
+                    <div className='flex gap-3 ml-5'>
                         <Link href='/'><li>Home</li></Link>
                         <Link href='/services'><li>Services</li></Link>
                         <Link href='/details'><li>Details</li></Link>
@@ -17,14 +23,12 @@ export default function Navbar() {
                         <Link href='blog'> <li>Blog</li></Link>
                         <Link href='contact'> <li>Contact</li></Link>
                     </div>
-                    <div className='flex justify-center items-center gap-8'>
-                        <Link href='login'><li>Log In</li></Link>
-                        <Link href='signup'><li><button type='button'>Sign Up</button></li></Link>
+                    <div className='flex flex-grow items-center justify-end'>
+                        <Link href='login'><li className='text-sky-600 font-semibold w-14'>Log in</li></Link>
+                        <Link href='signup'><li><button id='submit' type='button' className='boder shadow-md bg-slate-300 h-8 font-semibold text-[bg-gradient-to-r from-purple-600 to-blue-700] w-20 flex justify-center items-center rounded-lg'>Sign Up</button></li></Link>
                     </div>
 
                 </div>
-
-
             </ul>
         </nav>
     )
