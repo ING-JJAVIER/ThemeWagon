e477d7a6da34a17d5209a7e2c63dc057530fa9ac
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-17 07:12:15 -0600

    import

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 import Navbar from "@/components/Navbar";
-import Image  from 'next/image';
 
 export default function Home() {
   return (
