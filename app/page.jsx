543545a4b33caa7d10d9b323e5db4b2655014422
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-17 07:07:36 -0600

    import image change

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Navbar from "@/components/Navbar";
+import Image  from 'next/image';
 
 export default function Home() {
   return (
